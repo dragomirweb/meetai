99fa90bbb80e95119476687f738f27935ba0059c
Dan <dan@dragomirweb.com>
2025-06-09 22:21:05 +0300

    Configures user authentication
    Sets up better-auth with Drizzle ORM and PostgreSQL.
    
    This change configures user authentication using better-auth,
    drizzle-orm and postgres. It integrates the drizzle adapter for
    database interactions and enables email and password authentication.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,17 @@
-import * as schema from "@/db/schema";
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 
-import { betterAuth } from "better-auth";
-import { db } from "@/db"; // your drizzle instance
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from '@/db' // your drizzle instance
+import * as schema from '@/db/schema'
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "pg",
-        schema: {
-          ...schema,
-        }
-    }),
-    emailAndPassword: {
-        enabled: true,
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+    schema: {
+      ...schema,
     },
-});
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+})
