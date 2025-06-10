7635602af95c8f7f07d6fe50346bbd2213ac697b
Dan <dan@dragomirweb.com>
2025-06-10 16:54:15 +0300

    Adds generated avatar component
    Introduces a new component that dynamically generates avatars based on a seed string.
    
    It supports different avatar styles, offering flexibility in user representation.
    
    The component leverages the dicebear library for avatar generation and Radix UI primitives for rendering.

---
+++ src/components/ui/generated-avatar.tsx
@@ -0,0 +1,46 @@
+import {
+  botttsNeutral,
+  initials,
+} from '@dicebear/collection'
+import { createAvatar } from '@dicebear/core'
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from '@radix-ui/react-avatar'
+
+import { cn } from '@/lib/utils'
+
+type GeneratedAvatarProps = {
+  seed: string
+  className?: string
+  variant: 'botttsNeutral' | 'initials'
+}
+
+export const GeneratedAvatar = ({
+  seed,
+  className,
+  variant,
+}: GeneratedAvatarProps) => {
+  let avatar: ReturnType<typeof createAvatar>
+
+  if (variant === 'botttsNeutral') {
+    avatar = createAvatar(botttsNeutral, { seed })
+  } else {
+    avatar = createAvatar(initials, {
+      seed,
+      fontSize: 42,
+      fontWeight: 500,
+      radius: 100,
+    })
+  }
+
+  return (
+    <Avatar className={cn(className)}>
+      <AvatarImage src={avatar.toDataUri()} alt="Avatar" />
+      <AvatarFallback>
+        {seed.charAt(0).toUpperCase()}
+      </AvatarFallback>
+    </Avatar>
+  )
+}
