026835ee03b938e85bab7eced68d193ffa65fdb0
Dan <dan@dragomirweb.com>
2025-06-12 14:01:15 +0300

    add: agent form component for creating agents
    Implements a new form component to handle agent creation and editing.
    
    Utilizes React Hook Form with Zod for validation, and integrates with TRPC for data mutations.
    
    Provides user-friendly input fields for agent name and instructions, along with response handling for success and error scenarios.

---
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -0,0 +1,134 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import z from 'zod'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { GeneratedAvatar } from '@/components/ui/generated-avatar'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+
+import { agentsInsertSchema } from '@/modules/agents/schemas'
+import type { Agent } from '@/modules/agents/types'
+import { useTRPC } from '@/trpc/client'
+
+interface Props {
+  onSuccess?: () => void
+  onCancel?: () => void
+  initialValues?: Agent
+}
+
+export const AgentForm = ({ onSuccess, onCancel, initialValues }: Props) => {
+  const trpc = useTRPC()
+  const queryClient = useQueryClient()
+
+  const createAgent = useMutation(
+    trpc.agents.create.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(trpc.agents.getMany.queryOptions())
+
+        if (initialValues?.id)
+          await queryClient.invalidateQueries(
+            trpc.agents.getOne.queryOptions({
+              id: initialValues.id,
+            })
+          )
+
+        onSuccess?.()
+      },
+      onError: (err) => {
+        toast.error(err.message)
+        // TODO: check if error code is forbidden redirect to upgrade page
+      },
+    })
+  )
+
+  const form = useForm<z.infer<typeof agentsInsertSchema>>({
+    resolver: zodResolver(agentsInsertSchema),
+    defaultValues: {
+      name: initialValues?.name ?? '',
+      instructions: initialValues?.instructions ?? '',
+    },
+  })
+
+  const isEdit = !!initialValues?.id
+  const isPending = createAgent.isPending
+
+  const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
+    if (isEdit) {
+      console.log('TODO: update agent')
+    } else {
+      createAgent.mutate(values)
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        className="flex flex-col space-y-4"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <GeneratedAvatar
+          className="size-16 overflow-hidden rounded-full border"
+          seed={form.watch('name')}
+          variant="botttsNeutral"
+        />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="e.g. Math tutor" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="instructions"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Instructions</FormLabel>
+              <FormControl>
+                <Textarea
+                  {...field}
+                  placeholder="You are a helpful assistant..."
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex justify-between gap-x-2">
+          {onCancel && (
+            <Button
+              variant="ghost"
+              disabled={isPending}
+              type="button"
+              onClick={() => onCancel()}
+            >
+              Cancel
+            </Button>
+          )}
+          {onSuccess && (
+            <Button type="submit" disabled={isPending}>
+              {isEdit ? 'Update' : 'Create'}
+            </Button>
+          )}
+        </div>
+      </form>
+    </Form>
+  )
+}
