a89448c63c189eea39755d113c7fd57f9ca1b049
Dan <dan@dragomirweb.com>
2025-06-14 09:55:15 +0300

    add: define meetings type for bulk retrieval
    Adds a new type for retrieving multiple meetings from the API. This enhances type safety and improves code maintainability by clearly defining the structure of the data returned by the getMany endpoint.

--- src/modules/meetings/types.ts
+++ src/modules/meetings/types.ts
@@ -3,3 +3,5 @@ import { inferRouterOutputs } from '@trpc/server'
 import type { AppRouter } from '@/trpc/routers/_app'
 
 export type Meeting = inferRouterOutputs<AppRouter>['meetings']['getOne']
+export type Meetings =
+  inferRouterOutputs<AppRouter>['meetings']['getMany']['items']
