87e63de9303a1484ea61e5d05c12e948d78bec28
Dan <dan@dragomirweb.com>
2025-06-14 13:30:42 +0300

    add: meetings search filters component
    Implements a search filters component for meetings, allowing users to filter by name.
    
    Enhances user experience by integrating input handling and a search icon for improved usability.

---
+++ src/modules/meetings/ui/components/meetings-search-filters.tsx
@@ -0,0 +1,21 @@
+import { SearchIcon } from 'lucide-react'
+
+import { Input } from '@/components/ui/input'
+
+import { useMeetingsFilters } from '@/modules/meetings/hooks/use-meetings-filters'
+
+export const MeetingsSearchFilters = () => {
+  const [filters, setFilters] = useMeetingsFilters()
+
+  return (
+    <div className="relative flex items-center gap-x-2">
+      <Input
+        className="h-9 w-[200px] bg-white pl-7"
+        value={filters.search}
+        placeholder="Filter by name"
+        onChange={(e) => setFilters({ search: e.target.value })}
+      />
+      <SearchIcon className="text-muted-foreground absolute top-1/2 left-2 size-4 -translate-y-1/2" />
+    </div>
+  )
+}
