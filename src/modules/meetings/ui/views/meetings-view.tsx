bad3d8f89171d4a036e6a54f59d4b5fbc3141d27
Dan <dan@dragomirweb.com>
2025-06-14 09:56:12 +0300

    add: enhance meetings view with data table and empty state
    Implements a data table to display meeting items retrieved from the API.
    
    Adds an empty state component to guide users in creating their first meeting when there are no items available.

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -2,17 +2,32 @@
 
 import { useSuspenseQuery } from '@tanstack/react-query'
 
+import { DataTable } from '@/components/data-table'
+import { EmptyState } from '@/components/empty-state'
 import { ErrorState } from '@/components/error-state'
 import { LoadingState } from '@/components/loading-state'
 
 import { useTRPC } from '@/trpc/client'
+
+import { columns } from '../components/columns'
 
 export const MeetingsView = () => {
   const trpc = useTRPC()
 
   const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
 
-  return <div>data table</div>
+  return (
+    <div className="flex flex-1 flex-col gap-y-4 px-4 pb-4 md:px-8">
+      <DataTable data={data.items} columns={columns} />
+      {data.items.length === 0 && (
+        <EmptyState
+          title="Create your first meeting"
+          message="Schedule a meeting to connect woth other. Each meeting lets you collavorate, 
+           share ideas, and interact with participants in real time."
+        />
+      )}
+    </div>
+  )
 }
 
 export const MeetingsViewLoading = () => {
