8e7703726e59847706979b306293397c999567b3
Dan <dan@dragomirweb.com>
2025-06-15 14:14:35 +0300

    refactor: improve styling for call lobby component
    Updates the layout of the call lobby component to enhance readability by adjusting class names for better styling consistency.
    
    No functional changes introduced.

--- src/modules/call/ui/components/call-lobby.tsx
+++ src/modules/call/ui/components/call-lobby.tsx
@@ -57,7 +57,7 @@ export const CallLobby = ({ onJoin }: Props) => {
     <div className="from-sidebar-accent to-sidebar flex h-screen flex-grow flex-col items-center justify-center bg-radial">
       <div className="flex flex-1 items-center justify-center px-8 py-4">
         <div className="bg-background flex flex-col items-center justify-center gap-y-6 rounded-lg p-10 shadow-sm">
-          <div className="gap-y2 flex flex-col text-center">
+          <div className="flex flex-col gap-y-2 text-center">
             <h6 className="text-lg font-medium">Ready to join?</h6>
             <p className="text-sm"> Set up your call before joining</p>
           </div>
